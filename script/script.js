a7141407433b0c093d6ecc197d1a8830b23a26d9
Wesley Choonde Chilangi <wesleychilangi93@gmail.com>
2024-09-21 13:09:34 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
 // Function to display clock some where on the home page.
 function updateClock() {
     const clockElement = document.getElementById('clock');
-    const date = new Date();
-    const formattedTime = date.toLocaleString();
-    clockElement.textContent = formattedTime;
+    const now = new Date();
+    
+    // Get the current time in HH:MM:SS format
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    
+    // Display the time
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
+
 
 //This is an idea for future use.
 function changeBackgroundColor() {
